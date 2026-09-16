b27a400d752233b89ab99de968ba2013130892d5
Nabeera0-0 <nabeerafatima637@gmail.com>
2026-09-16 23:21:54 +0500

    Create task 7.c

---
+++ task 7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    float num1,num2,num3;
+    float sum, avg;
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    printf("Enter third number: ");
+    scanf("%f", &num3);
+    sum=num1+num2+num3;
+    avg=sum/3;
+    printf("Sum is %f\n", sum);
+    printf("Average is %f\n", avg);
+
+    return 0 ;
+}
